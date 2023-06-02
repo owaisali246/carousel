dfdbc5c6f662def64757dffcb0f81c06eda3f444
Syed Owais Ali <owaisali246.soa@gmail.com>
2023-06-02 23:42:58 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Carousel from "react-elastic-carousel";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Item from "./Items";
+import "./App.css";
+const img_blogs = require("./images/Blog.png");
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 1 },
+  { width: 768, itemsToShow: 1 },
+  { width: 1200, itemsToShow: 1 },
+];
+
+const content = [
+  { image: img_blogs },
+  { image: img_blogs },
+  { image: img_blogs },
+  { image: img_blogs },
+  { image: img_blogs },
+  { image: img_blogs },
+  { image: img_blogs },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      {/* <div className="team_heading">
+        <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
+      </div>
+       */}
+      <Carousel
+        autoPlay={true}
+        showThumbs={false}
+        showArrows={false}
+        className={"carouselHome"}
+        showStatus={false}
+        centerMode={true}
+        centerSlidePercentage={100}
+        infiniteLoop={true}
+      >
+        {content.map((img, index) => {
+          return (
+            <div key={index} className="carousel_item">
+              <img src={img.image} alt="no-text" className="carousel_img" />
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
